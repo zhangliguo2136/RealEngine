ef8de1dc2f044fe3d911c108d95c6a99414033cf
zlg <947673853@qq.com>
2021-02-01 21:11:09 +0800

    完善线程池（未加锁）

--- RealEngine/RealEngine/ThreadPool.cpp
+++ RealEngine/RealEngine/ThreadPool.cpp
@@ -1,27 +1,69 @@
 #include "ThreadPool.h"
 
-RealEngine::ThreadPool::ThreadPool() 
+RealEngine::ThreadPool::ThreadPool():b_started(false)
 {
-	int coreCount = std::thread::hardware_concurrency();
-	threads.reserve(coreCount);
 }
 
 RealEngine::ThreadPool::~ThreadPool() 
 {
+	if (b_started) 
+	{
+		stop();
+	}
+}
 
+void RealEngine::ThreadPool::start()
+{
+	int coreCount = std::thread::hardware_concurrency();
+	_threads.reserve(coreCount);
+
+	for (int i = 0; i < coreCount; i++) 
+	{
+		_threads.push_back(new std::thread(std::bind(&ThreadPool::onThreadAllocator, this)));
+	}
+
+	b_started = true;
+}
+
+void RealEngine::ThreadPool::stop()
+{
+	b_started = false;
+
+	for (std::vector<std::thread*>::iterator iter = _threads.begin(); iter != _threads.end(); iter++)
+	{
+		(*iter)->join();
+		delete *iter;
+	}
+
+	_threads.clear();
 }
 
 void RealEngine::ThreadPool::addThreadTask(std::function<void()> func) 
 {
-	tasks.push_back(func);
+	_tasks.push(func);
 }
 
 
-void RealEngine::ThreadPool::nextTask()
+std::function<void()> RealEngine::ThreadPool::nextTask()
 {
+	std::function<void()> _func = nullptr;
+	if (!_tasks.empty() && b_started)
+	{
+		_func = _tasks.front();
+		_tasks.pop();
+	}
 
+	return _func;
 }
-void RealEngine::ThreadPool::threadAllocator() 
+void RealEngine::ThreadPool::onThreadAllocator() 
 {
+	while (b_started) 
+	{
+		std::function<void()> func = nextTask();
 
+		if (func) 
+		{
+			func();
+		}
+	}
 }
--- RealEngine/RealEngine/ThreadPool.h
+++ RealEngine/RealEngine/ThreadPool.h
@@ -2,6 +2,7 @@
 #include <functional>
 #include <Thread>
 #include <vector>
+#include <queue>
 
 namespace RealEngine
 {
@@ -12,15 +13,18 @@ namespace RealEngine
 		~ThreadPool();
 
 		void addThreadTask(std::function<void()> func);
-
+		void start();
+		void stop();
 	private:
 		// 任务分配函数
-		void threadAllocator();
-		void nextTask();
+		void onThreadAllocator();
+		std::function<void()> nextTask();
 
 		// 线程
-		std::vector<std::thread> threads;
+		std::vector<std::thread*> _threads;
 		// 线程任务
-		std::vector<std::function<void()>> tasks;
+		std::queue<std::function<void()>> _tasks;
+
+		bool b_started;
 	};
 }
