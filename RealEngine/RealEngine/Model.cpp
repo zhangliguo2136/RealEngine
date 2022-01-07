d1c21adec239fd581a13fcd4ab722b52a6c34645
zlg <947673853@qq.com>
2022-01-07 23:51:03 +0800

    添加assimp 库

--- RealEngine/RealEngine/Model.cpp
+++ RealEngine/RealEngine/Model.cpp
@@ -10,6 +10,10 @@
 #include "Shader.h"
 #include "Image.h"
 
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
 
 Model::Model(const std::string &filename)
 {
